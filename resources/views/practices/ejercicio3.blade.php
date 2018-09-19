6329a763c33fcb1175c9a83caad670185047929d
IsaacLf <victormanuelarena@hotmail.com>
2018-09-19 11:37:15 -0500

    Ejercicio 3

--- resources/views/practices/ejercicio3.blade.php
+++ resources/views/practices/ejercicio3.blade.php
@@ -8,6 +8,36 @@
     <link rel="stylesheet" type="text/css" media="screen" href="{{asset('css/practices/style3.css')}}" />
 </head>
 <body>
-    <h1>Este es un ejemplo para la maestra Yaqui</h1>
+    <h1>Lista de imágenes</h1>
+    <table class="image-list">
+        <tr>
+            <th>Nombre (Título de la imagen)</th>
+            <th>Imagen</th>
+        </tr>
+        <tr>
+            <td>Imagen 1</td>
+            <td class="center-image">
+                <img src="{{asset('img/image1.jpg')}}" alt="name1" onclick="location.href='/practica/public/img/image1.jpg';">
+            </td>
+        </tr>
+        <tr>
+            <td>Imagen 2</td>
+            <td class="center-image">
+                <img src="{{asset('img/image2.jpg')}}" alt="name2" onclick="location.href='/practica/public/img/image2.jpg';">
+            </td>
+        </tr>
+        <tr>
+            <td>Imagen 3</td>
+            <td class="center-image">
+                <img src="{{asset('img/image3.jpg')}}" alt="name3" onclick="location.href='/practica/public/img/image3.jpg';">
+            </td>
+        </tr>
+        <tr>
+            <td>Imagen 4</td>
+            <td class="center-image">
+                <img src="{{asset('img/image4.jpg')}}" alt="name4" onclick="location.href='/practica/public/img/image4.jpg';">
+            </td>
+        </tr>
+    </table>
 </body>
 </html>
